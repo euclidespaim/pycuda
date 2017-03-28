0de2d37024dce9d41d5a28fb8af149b8a82f86af
euclidespaim <euclidespaim@gmail.com>
2017-03-28 18:15:48 -0300

    algortimo base do projeto mestrado

--- Python Sources/algoritmo.py
+++ Python Sources/algoritmo.py
@@ -1,30 +1,36 @@
 import gzip
+import subprocess
 from threading import Thread
 
 class NetworkTest(object):
 
     threads=[]
 
-    def transmissao(self):
+    def transmissao(upl,):
+        downl = subprocess.getoutput('speedtest-cli --csv')
+        '''upl = subprocess.getoutput('speedtest-cli --csv')'''
         print("transmitindo...")
 
+        print(downl)
+        print(upl)
 
 
     def compressao(self):
         content = b"Lots of content here"
         with gzip.open('c:/Users/kid/desktop/up/file.txt.gz', 'wb') as f:
             f.write(content)
+
         print('entrou...')
 
 def main ():
-    rede = 0
+    print ('Realizando teste de rede...')
 
     scan = NetworkTest()
     scan.transmissao()
 
-    if ( rede < 10):
-        zip = NetworkTest()
-        zip.compressao()
+    '''if len(upl) < 10:'''
+    zip = NetworkTest()
+    zip.compressao()
 
     return 0
 if __name__ == '__main__':

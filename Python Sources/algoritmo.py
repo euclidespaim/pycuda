4a1bd21d7f00248cb611ee8a731d427a70cc1a78
euclidespaim <euclidespaim@gmail.com>
2017-03-29 08:44:34 -0300

    Algortimo base do projeto mestrado funcionandogit status

--- Python Sources/algoritmo.py
+++ Python Sources/algoritmo.py
@@ -1,37 +1,62 @@
 import gzip
 import subprocess
+import re
+import threading
 from threading import Thread
 
 class NetworkTest(object):
 
     threads=[]
 
-    def transmissao(upl,):
-        downl = subprocess.getoutput('speedtest-cli --csv')
+    def transmissao(down=int):
+        print("Testando transmissao...")
+        downl = str(subprocess.getoutput('speedtest-cli --simple'))
+        print (downl)
+        '''print([x for x in downl().iterint()])'''
+        down = (re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", downl))
         '''upl = subprocess.getoutput('speedtest-cli --csv')'''
-        print("transmitindo...")
 
-        print(downl)
-        print(upl)
+        return down
 
 
     def compressao(self):
-        content = b"Lots of content here"
-        with gzip.open('c:/Users/kid/desktop/up/file.txt.gz', 'wb') as f:
-            f.write(content)
+        try:
+            content = b"Lots of content here"
+            with gzip.open('c:/Users/kid/desktop/up/file.txt.gz', 'wb') as f:
+                f.write(content)
+                print('Compressao de nivel 0 aplicada...')
+        except:
+            print('Algo deu errado!!')
 
-        print('entrou...')
+    def compress(self):
+        try:
+            content = b"Lots of content here"
+            with gzip.open('c:/Users/kid/desktop/up/file.txt.gz', 'wb') as f:
+                f.write(content)
+                print('Compressao de nível 0 aplicada...')
+        except:
+            print('Algo saiu errado aqui!')
 
-def main ():
-    print ('Realizando teste de rede...')
+def main (transmissao=None, compressao=None, compress=None):
+    print ('Realizando testes na rede...')
 
-    scan = NetworkTest()
-    scan.transmissao()
+    t = threading.Thread(name='transmissao', target= transmissao)
+    c1 = threading.Thread(name='compressao', target=compressao)
+    c2 = threading.Thread(name='compress', target=compress)
+    t.start()
 
-    '''if len(upl) < 10:'''
-    zip = NetworkTest()
-    zip.compressao()
+    lista = NetworkTest.transmissao()
+    print (lista[1])
+    test = float(lista[1])
+
+    if test < 50:
+        c1.start()
+        print('Compressao de nivel 1 aplicada...')
+    else:
+        c2.start()
+        print('Compressao de nivel 2 aplicada...')
 
     return 0
+
 if __name__ == '__main__':
     main()

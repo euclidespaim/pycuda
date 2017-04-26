5cd17ea6c18b1ab4616504aad1af10a14eac21ee
euclidespaim <euclidespaim@gmail.com>
2017-04-26 09:17:47 -0300

    Created virtual enviroment pyCuda

--- Python Sources/main/zipthread.py
+++ Python Sources/main/zipthread.py
@@ -20,13 +20,13 @@ class AsyncZip(threading.Thread):
 
         return down
 
-    def run(self):
+    def runz(self, outfile):
         f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
         f.write(self.infile)
         f.close()
         print ('Finished background zip of: ', self.infile)
 
-    def zip(self):
+    def zipz(self, outfile):
         z = zipfile.ZipFile(self.outfile, 'w', zipfile.zipfile.ZIP_BZIP2)
         z.write(self.infile)
         z.close()
@@ -44,14 +44,14 @@ test = float(lista[2])
 
 if test < 5:
     Tk().withdraw()
-    background = AsyncZip.run(askopenfilename(), 'C:\\Users\Kid\\Desktop\\up\\myarchive.gz')
+    background = AsyncZip.runz(askopenfilename(), 'C:\\Users\Kid\\Desktop\\up\\myarchive.gz')
     background.start()
     print ('The main program continues to run in foreground.')
     background.join()    # Wait for the background task to finish
     print ('Compressao gzip aplicada.')
 else:
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    background = AsyncZip.zip(askopenfilename(), 'C:\\Users\Kid\\Desktop\\up\\myarchive.bz')
+    background = AsyncZip.zipz(askopenfilename(), 'C:\\Users\Kid\\Desktop\\up\\myarchive.bz')
     background.start()
     print('Compress bz2 aplicada.')
     background.join()

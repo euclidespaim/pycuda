4df3765ef816145f944806a9d92888642cb1cde7
euclidespaim <euclidespaim@gmail.com>
2017-04-27 15:10:27 -0300

    If case + threads!

--- Python Sources/main/algoritmo.py
+++ Python Sources/main/algoritmo.py
@@ -7,6 +7,26 @@ import threading
 import time
 from tkinter.filedialog import *
 
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        """Return the match method once, then stop"""
+        yield self.match
+        raise StopIteration
+
+    def match(self, *args):
+        """Indicate whether or not to enter a case suite"""
+        if self.fall or not args:
+            return True
+        elif self.value in args: # changed for v1.5, see below
+            self.fall = True
+            return True
+        else:
+            return False
+
 
 class NetworkTest(object):
 
@@ -20,23 +40,29 @@ class NetworkTest(object):
         return down
 
     def gzipcomp():
-        print('Iniciando compressão gzip...')
-        f_in = open(askopenfilename(), 'rb')
-        f_out = gzip.open('C:\\Users\Kid\\Desktop\\up\\Archive.gz', 'wb', 9)
-        f_out.writelines(f_in)
-        f_out.close()
-        f_in.close()
-        print('Compressao gzip aplicada...')
+        try:
+            print('Iniciando compressão gzip...')
+            f_in = open(askopenfilename(), 'rb')
+            f_out = gzip.open('C:\\Users\Kid\\Desktop\\up\\Archive.gz', 'wb', 9)
+            f_out.writelines(f_in)
+            f_out.close()
+            f_in.close()
+            print('Compressao gzip aplicada...')
+        except ValueError:
+            print ("Oops!  That was no valid number.  Try again...")
         return  f_out
 
     def zlibcomp():
-        print('Iniciando compressão zlib...')
-        f_in = open(askopenfilename(), 'rb')
-        f_out = bz2.open('C:\\Users\Kid\\Desktop\\up\\Archive.bz', 'w', 9)
-        f_out.writelines(f_in)
-        f_out.close()
-        f_in.close()
-        print('Compressao zlib aplicada...')
+        try:
+            print('Iniciando compressão zlib...')
+            f_in = open(askopenfilename(), 'rb')
+            f_out = bz2.open('C:\\Users\Kid\\Desktop\\up\\Archive.bz', 'w', 9)
+            f_out.writelines(f_in)
+            f_out.close()
+            f_in.close()
+            print('Compressao zlib aplicada...')
+        except ValueError:
+            print ("Oops!  That was no valid number.  Try again...")
         return  f_out
 
 def main (transmissao=None, gzipcomp=None, zlibcomp=None):
@@ -45,20 +71,33 @@ def main (transmissao=None, gzipcomp=None, zlibcomp=None):
 
     t = threading.Thread(name='t', target=transmissao)
     lista = NetworkTest.transmissao()
-    print (lista[1])
-    test = float(lista[1])
+    f = float(lista[2])
+    print ('float: ', f)
+    v = int(f)
+    print ('int: ', v)
 
-    if test < 65:
-        c = threading.Thread(name='gzipcomp', target=NetworkTest.gzipcomp())
-        print (threading.currentThread().getName() , 'Starting')
-        time.sleep(2)
-        print('Compressao de nivel 1 aplicada...')
-    else:
-        z = threading.Thread(name='zlibcomp', target=NetworkTest.zlibcomp())
-        print (threading.currentThread().getName(), 'Starting')
-        time.sleep(2)
-        print('Compressao de nivel 2 aplicada...')
 
+    for case in switch(v):
+        if case(5):
+            c = threading.Thread(name='gzipcomp', target=NetworkTest.gzipcomp())
+            print (threading.currentThread().getName() , 'Starting')
+            time.sleep(2)
+            print('Compressao de nivel 1 aplicada...')
+            break
+
+        if case(4):
+            z = threading.Thread(name='zlibcomp', target=NetworkTest.zlibcomp())
+            print (threading.currentThread().getName(), 'Starting')
+            time.sleep(2)
+            print('Compressao de nivel 2 aplicada...')
+            break
+
+        if case(3):
+            z = threading.Thread(name='zlibcomp', target=NetworkTest.zlibcomp())
+            print (threading.currentThread().getName(), 'Starting')
+            time.sleep(2)
+            print('Compressao de nivel 3 aplicada...')
+            break
 
 if __name__ == '__main__':
     main()
